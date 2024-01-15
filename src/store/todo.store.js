4be43d5724e59fbb80d939c24aae9fba33603136
Edison de la Cruz <edison.icr@gmail.com>
2024-01-15 14:28:17 +0100

    feat(todo): implementar stores

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -4,7 +4,7 @@ const Filters = {
     All: 'all',
     Completed: 'Completed',
     Pending: 'Pending'
-}
+};
 
 const state = {
     todos: [
@@ -13,13 +13,86 @@ const state = {
         new Todo('Comer'),
     ],
     filter: Filters.All,
-}
+};
 
 const initStore = () => {
     console.log(state);
     console.log('InitStore {{Edi/Byte}} 💻');
 }
 
+const loadStore = () => {
+    throw new Error('No implementado');
+}
+
+const getTodos = ( filter = Filters.All ) => {
+    switch ( filter ) {
+        case Filters.All:
+            return [...state.todos];
+        case Filters.Completed:
+            return state.todos.filter( todo => todo.done);
+        case Filters.Pending:
+            return state.todos.filter( todo => !todo.done);
+        default:
+            throw new Error(`La opción ${filter} no es valida.`);
+            break;
+    }
+}
+
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo = ( description ) => {
+    if(!description) throw new Error('Descripción requerida');
+    state.todos.push( new Todo(description) );
+}
+
+/**
+ * 
+ * @param {String} todoId Identificador del todo
+ */
+const toggleTodo = ( todoId ) => {
+    state.todos = state.todos.map( todo => {
+        if ( todo.id === todoId ) {
+            todo.done = !toolbar.done;
+        }
+        return todo;
+    });
+}
+/**
+ * 
+ * @param {String} todoId Identificador del todo
+ */
+const deleteTodo = ( todoId ) => {
+    if(!todoId) throw new Error('Id requerido');
+    state.todos = state.todos.filter( todo => todo.id !== todo );
+}
+
+const deleteCompleted = () => {
+    state.todos = state.todos.filter( todo => todo.done );
+}
+
+/**
+ * 
+ * @param {Filters} newFilter All | Completed | Pending
+ */
+const setFilter = ( newFilter = Filters.All ) => {
+    if( !Object.keys(Filters).includes(newFilter) ) throw new Error('Filtro invalido');
+    state.filter = newFilter;
+}
+
+const getCurrentFilter = () => {
+    return state.filter;
+}
+
 export default {
-    initStore
+    addTodo,
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
+    getTodos,
+    initStore,
+    loadStore,
+    setFilter,
+    toggleTodo,
 }
